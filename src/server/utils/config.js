c63852fb6e73b913c8399f05596dc9bdae16baa7
Nathan Kunicki <me@nathankunicki.com>
2015-04-25 18:56:16 +0100

    Logging for config

--- src/server/utils/config.js
+++ src/server/utils/config.js
@@ -6,13 +6,15 @@ var Config = module.exports = (function () {
     var configFile = process.env.CONFIG_FILE || "config.dev.json",
         config = JSON.parse(fs.readFileSync("./config/" + configFile));
 
-    console.log("Loaded " + configFile);
 
     config.port = process.env.PORT || config.port || 8080;
 
     config.db.hostname = process.env.DB_HOSTNAME || config.db.hostname || "localhost";
     config.db.username = process.env.DB_USERNAME || config.db.username;
     config.db.password = process.env.DB_PASSWORD || config.db.password;
+
+    console.log("Loaded " + configFile + ":");
+    console.log(config);
 
     return config;
 
