b59b162a4f1cb201d29c8962a627c93f998baeb3
Nathan Kunicki <me@nathankunicki.com>
2015-04-25 19:00:35 +0100

    Updated configs

--- src/server/utils/config.js
+++ src/server/utils/config.js
@@ -10,6 +10,8 @@ var Config = module.exports = (function () {
     config.port = process.env.PORT || config.port || 8080;
 
     config.db.hostname = process.env.DB_HOSTNAME || config.db.hostname || "localhost";
+    config.db.port = process.env.DB_PORT || config.db.port || 3306;
+    config.db.database = process.env.DB_DATABASE || config.db.database || "rewind";
     config.db.username = process.env.DB_USERNAME || config.db.username;
     config.db.password = process.env.DB_PASSWORD || config.db.password;
 
